183f929efeaa98d9ab7b8f8225e91ef7973b2658
Ishwar Chudhary <ishwarura@gmail.com>
2023-10-01 22:01:55 +0545

    Update simple_interest.dart

--- flutter_class/lib/views/simple_interest.dart
+++ flutter_class/lib/views/simple_interest.dart
@@ -1,16 +1,88 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class SimpleInterest extends StatefulWidget {
-  const SimpleInterest({super.key});
+  const SimpleInterest({Key? key}) : super(key: key);
 
   @override
   State<SimpleInterest> createState() => _SimpleInterestState();
 }
 
 class _SimpleInterestState extends State<SimpleInterest> {
+  final TextEditingController _principalAmountController =
+      TextEditingController();
+  final TextEditingController _rateOfInterestController =
+      TextEditingController();
+  final TextEditingController _timePeriodController = TextEditingController();
+
+  double _result = 0.0;
+
+  void _calculateSI() {
+    setState(() {
+      double principalAmount = double.parse(_principalAmountController.text);
+      double rateOfInterest = double.parse(_rateOfInterestController.text);
+      double timePeriod = double.parse(_timePeriodController.text);
+
+      _result = (principalAmount * rateOfInterest * timePeriod) / 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 43, 85, 159),
+        elevation: 0,
+        title: const Text('Simple Interest'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _principalAmountController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Principal Amount',
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: _rateOfInterestController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Rate of Interest',
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: _timePeriodController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Time Period',
+                ),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Result: $_result',
+                style: const TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _calculateSI();
+                  },
+                  child: const Text('CALCULATE'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
