44c12684c8cd47e1892655e7941bf67f62d50e4c
Ishwar Chudhary <ishwarura@gmail.com>
2023-10-01 22:23:08 +0545

    Update area_of_circle.dart

--- flutter_class/lib/views/area_of_circle.dart
+++ flutter_class/lib/views/area_of_circle.dart
@@ -1,16 +1,63 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class AreaOfCircle extends StatefulWidget {
-  const AreaOfCircle({super.key});
+  const AreaOfCircle({Key? key}) : super(key: key);
 
   @override
-  State<AreaOfCircle> createState() => _AreaOfCircleState();
+  _AreaOfCircleState createState() => _AreaOfCircleState();
 }
 
 class _AreaOfCircleState extends State<AreaOfCircle> {
+  final TextEditingController _radiusController = TextEditingController();
+
+  double _result = 0.0;
+
+  void _calculateArea() {
+    setState(() {
+      double radius = double.parse(_radiusController.text);
+      _result = 3.14 * radius * radius;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 43, 85, 159),
+        elevation: 0,
+        title: const Text('Area of Circle'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _radiusController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Radius',
+                ),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Result: $_result',
+                style: const TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _calculateArea();
+                  },
+                  child: const Text('Calculate'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
